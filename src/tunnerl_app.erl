680115161ddfc347ec9c454f8064cbf39e8a083c
Yury Gargay <yury.gargay@gmail.com>
2016-05-10 16:10:51 +0600

    Set missed auth callback module

--- src/tunnerl_app.erl
+++ src/tunnerl_app.erl
@@ -13,7 +13,8 @@ start(_StartType, _StartArgs) ->
     Port = application:get_env(tunnerl, port, 1080),
     Auth = application:get_env(tunnerl, auth, [0]),
     Proto = application:get_env(tunnerl, protocols, 1080),
-    Opts = [{auth, Auth}, {protocols, Proto}],
+    AuthMod = application:get_env(tunnerl, auth_module, tunnerl_auth_dummy),
+    Opts = [{auth, Auth}, {protocols, Proto}, {auth_module, AuthMod}],
     {ok, _} = ranch:start_listener(tunnerl, 10, ranch_tcp, [{port, Port}], socks_protocol, Opts),
     tunnerl_sup:start_link().
 
